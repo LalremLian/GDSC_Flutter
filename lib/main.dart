31020439be9b05c1e236a9e0ff2cb6a12b0eea32
LalremLian <amongbt@gmail.com>
2022-03-15 17:36:23 +0600

    Worked on Profile Stateless

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application................................
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("Project 'F'"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Image.network(
+              "https://pfpmaker.com/_nuxt/img/profile-3-1.3e702c5.png",
+              height: 200,
+              width: 320),
+          const SizedBox(
+            height: 15,
+          ),
+          const Text(
+            "Me Minion",
+            style: TextStyle(
+                fontSize: 35, fontWeight: FontWeight.bold, color: Colors.amber),
+          ),
+          const Text(
+            "Me want Banana!",
+            style: TextStyle(fontSize: 20, color: Colors.orangeAccent),
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 40,
+              color: Colors.white70,
+              child: Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.account_circle),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("User Name"),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 40,
+              color: Colors.white70,
+              child: Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.account_circle),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("User Name"),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 20,
+            width: 380,
+            color: Colors.deepPurple,
+          ),
+          Container(
+            height: 20,
+            width: 380,
+            color: Colors.blueAccent,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Profile()),
+                );
+              },
+              child: const Text('Submit'))
+        ],
+      ),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: const Text(
+          'Profile',
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton( //Navigator.pop is not mandatory......................
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go Back'),
+          ),
+        ],
+      ),
+    );
+  }
+}
