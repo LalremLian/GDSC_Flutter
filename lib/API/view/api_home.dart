da8eda0cd907cad43895655afb81a6030c806811
LalremLian <amongbt@gmail.com>
2022-06-25 16:59:07 +0600

    Worked on apiHome

--- lib/API/view/api_home.dart
+++ lib/API/view/api_home.dart
@@ -2,7 +2,8 @@ import 'package:dummy/API/model/post_model.dart';
 import 'package:dummy/API/service/remote_services.dart';
 import 'package:dummy/API/view/api_profile.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
+
+import '../widgets/ShimeringWidget.dart';
 
 class ApiHome extends StatefulWidget {
   const ApiHome({Key? key}) : super(key: key);
@@ -97,118 +98,7 @@ class _ApiHomeState extends State<ApiHome> {
               );
             },
         ),
-        replacement: Center(
-          child: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Expanded(child:
-                Shimmer.fromColors(
-                    baseColor: Colors.grey.shade300,
-                    highlightColor: Colors.grey.shade100,
-                    child: ListView.builder(
-                        itemBuilder: (_, __) => Padding(
-                            padding: const EdgeInsets.only(bottom: 15),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(40),
-                                child: Container(
-                                  width: 80.0,
-                                  height: 80.0,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                              ),
-                              Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Container(
-                                        width: double.infinity,
-                                        height: 12.0,
-                                        color: Colors.white,
-                                      ),
-                                      const Padding(
-                                        padding: EdgeInsets.symmetric(vertical: 2.0),
-                                      ),
-                                      Row(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Container(
-                                              width: 70.0,
-                                            height: 12.0,
-                                            color: Colors.white,
-                                          ),
-                                          const Padding(
-                                            padding: EdgeInsets.symmetric(horizontal: 2.0),
-                                          ),
-                                          Expanded(
-                                            child: Container(
-                                              width: double.infinity,
-                                              height: 12.0,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          const Padding(
-                                            padding: EdgeInsets.symmetric(vertical: 8.0),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Container(
-                                              width: 180.0,
-                                            height: 12.0,
-                                            color: Colors.white,
-                                          ),
-                                          const Padding(
-                                            padding: EdgeInsets.symmetric(horizontal: 2.0),
-                                          ),
-                                          Expanded(
-                                            child: Container(
-                                              width: double.infinity,
-                                              height: 12.0,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                          const Padding(
-                                            padding: EdgeInsets.symmetric(vertical: 8.0),
-                                          ),
-                                        ],
-                                      ),
-                                      Container(
-                                        width: double.infinity,
-                                        height: 12.0,
-                                        color: Colors.white,
-                                      ),
-                                      const Padding(
-                                        padding: EdgeInsets.symmetric(vertical: 2.0),
-                                      ),
-                                      Container(
-                                        width: 40.0,
-                                        height: 12.0,
-                                        color: Colors.white,
-                                      ),
-                                    ],
-                                  ))
-                            ],
-                          ),
-                        ),
-                      itemCount: 8,
-                    ),
-                ),
-                ),
-              ],
-            ),
-          )
-        ),
+        replacement: const ShimmerWidget(),
       ),
     );
   }
@@ -224,3 +114,5 @@ class _ApiHomeState extends State<ApiHome> {
       }
   }
 }
+
+
